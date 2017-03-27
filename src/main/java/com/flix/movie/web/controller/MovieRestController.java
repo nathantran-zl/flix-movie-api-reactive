4bb5366774362bbc35c61e723c2673fd110abbe1
Steven Romero <sromero@mbp-sromero.local>
2017-03-27 15:09:38 -0400

    finished contronller layer

--- src/main/java/com/flix/movie/web/controller/MovieRestController.java
+++ src/main/java/com/flix/movie/web/controller/MovieRestController.java
@@ -1,10 +1,74 @@
 package com.flix.movie.web.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.flix.movie.entity.Movie;
+import com.flix.movie.service.MovieService;
+import com.flix.movie.web.model.request.MovieRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 /**
  * Created by sromero on 2/20/17.
  */
 @RestController
 public class MovieRestController {
+
+    @Autowired
+    private MovieService movieService;
+
+    @GetMapping(value = "/movies")
+    public Flux<ResponseEntity<Movie>> list() {
+
+        return movieService.list().map(m -> new ResponseEntity<>(m, HttpStatus.OK));
+    }
+
+    @GetMapping(value = "/moviesByRating")
+    public Flux<ResponseEntity<Movie>> findByRating(
+            @RequestParam(value = "rating", required = false) final String rating) {
+
+        return movieService.findByRating(rating)
+                .map(m -> new ResponseEntity<>(m, HttpStatus.OK));
+
+    }
+
+    @GetMapping("/movies/{movieId}")
+    public Mono<ResponseEntity<Movie>> read(
+            @PathVariable("movieId") final String movieId) {
+
+        return movieService.read(movieId)
+                .map(m -> new ResponseEntity<>(m, HttpStatus.OK));
+    }
+
+    @DeleteMapping("/movies/{movieId}")
+    public Mono<ResponseEntity<Movie>> delete(
+            @PathVariable("movieId") final String movieId) {
+
+        return movieService.delete(movieId)
+                .map(m -> new ResponseEntity<>(m, HttpStatus.OK));
+    }
+
+    @PutMapping("/movies/{movieId}")
+    public Mono<ResponseEntity<Movie>> update(
+            @PathVariable("movieId") final String movieId,
+            @RequestBody final MovieRequest movieRequest) {
+
+        return movieService.update(movieId, movieRequest)
+                .map(m -> new ResponseEntity<>(m, HttpStatus.OK));
+
+    }
+
+    @PostMapping("/movies")
+    public Mono<ResponseEntity<Movie>> create(
+            @RequestBody final Mono<MovieRequest> movieRequest) {
+
+        return movieService.create(movieRequest)
+                .map(m -> new ResponseEntity<>(m, HttpStatus.OK));
+
+    }
+
 }
